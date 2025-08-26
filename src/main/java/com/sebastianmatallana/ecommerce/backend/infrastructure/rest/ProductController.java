49fe4bc5ec1ec70f65f601ee40be151a95f9ce23
SebastianMatallana <sebastian.matallana99@gmail.com>
2025-08-26 04:29:08 -0500

    Endpoint fixes for saving products

--- src/main/java/com/sebastianmatallana/ecommerce/backend/infrastructure/rest/ProductController.java
+++ src/main/java/com/sebastianmatallana/ecommerce/backend/infrastructure/rest/ProductController.java
@@ -9,18 +9,42 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
 @RestController
 //http://localhost:8080
 @RequestMapping("/api/v1/admin/products")
 //http://localhost:8080/api/v1/admin/products
 @Slf4j
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200/")
 public class ProductController {
 
     private final ProductService productService;
 
     @PostMapping
-    public ResponseEntity<Product> save(@RequestBody Product product) {
+    public ResponseEntity<Product> save(@RequestParam("id") Integer id,
+                                        @RequestParam("code") String code,
+                                        @RequestParam("name") String name,
+                                        @RequestParam("description") String description,
+                                        @RequestParam("price")BigDecimal price,
+                                        @RequestParam("urlImage")String urlImage,
+                                        @RequestParam("userId")Integer userId,
+                                        @RequestParam("categoryId")Integer categoryId) {
+        Product product = new Product();
+        if (id != null && id > 0) {
+            product.setId(id);
+        }
+        product.setCode(code);
+        product.setName(name);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setCategoryId(categoryId);
+        product.setUserId(userId);
+        product.setUrlImage(urlImage);
+
+
+
         log.info("Nombre producto {}", product.getName());
         return new ResponseEntity<>(this.productService.save(product), HttpStatus.CREATED);
     }
